0afd05e00673ecfbfca570f8e1987104fe57c0ba
Excelr Solutions <mailmeasradha@gmail.com>
2025-09-12 16:57:48 +0530

    variables

---
+++ dsa/basics/Container.java
@@ -0,0 +1,27 @@
+class Container{
+   public static void main(String[]args){
+      String name ="Radha";
+      int age=34;
+      long  phone=623456352632l;
+      double height =5.4;
+      char gender='F';
+      boolean married =false;
+     System.out.println(name);
+     System.out.println(age);
+
+   }
+}
+
+
+
+
+/*
+   1.javac FileName.java  
+   compiler will compile the code and gives       bytecode as ____.clas
+   it will check syntax error
+   main method is not mandatory
+   2.java ClassName
+   it executes program
+   main method is mandatory
+
+*/
