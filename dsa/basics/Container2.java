410297ffa09636712960c2de685c660246a79747
Excelr Solutions <mailmeasradha@gmail.com>
2025-09-15 16:47:06 +0530

    tags

---
+++ dsa/basics/Container2.java
@@ -0,0 +1,23 @@
+class Container2{
+
+ public static void main(String[]args){
+    int value1=10;
+    System.out.println(value1);//10
+ // int value1=20;   value1 is already defined/declared
+  
+    value1=20; //re -assign or re-initliziation
+    System.out.println(value1);//20
+    
+    String value2;  // variable declaration
+    //  System.out.println(value2);--Error value2 is not assigned
+    value2="radha";  // variable initliziation
+   System.out.println(value2);
+   value2="bhaskar"; // variable re-initlization
+   System.out.println(value2);
+
+
+
+
+ }
+
+}
