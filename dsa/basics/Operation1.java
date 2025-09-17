c5d9fc387661e7683c43a7ee500a72b38d3b4d93
Excelr Solutions <mailmeasradha@gmail.com>
2025-09-17 21:55:08 +0530

    flex tasks

---
+++ dsa/basics/Operation1.java
@@ -0,0 +1,27 @@
+class Operation1{
+ public static void main(String[]args){
+    int a=20;
+    int b=10;
+//  Arithmetic Operations
+    System.out.println(a+b);//30
+    System.out.println(a-b);//-10
+    System.out.println(a*b);//200
+    System.out.println(a/b);//2 -Quotient
+    System.out.println(a%b);//0 -reminder(modules)
+
+//Comparision Operator--boolean
+ 
+  System.out.println(10>20);//false
+  System.out.println(10<20);//true
+  System.out.println(10>=10);//true
+  System.out.println(10<=10);//true
+  System.out.println(10==20);//false
+  System.out.println(10!=20);//true
+
+
+
+
+
+}
+
+}
