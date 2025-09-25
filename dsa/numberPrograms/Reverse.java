bdcfbca4e4207d6c1e46cdbface8357c35194e54
Excelr Solutions <mailmeasradha@gmail.com>
2025-09-25 16:59:57 +0530

    js introduction and operators

--- dsa/numberPrograms/Reverse.java
+++ dsa/numberPrograms/Reverse.java
@@ -4,7 +4,7 @@ public class Reverse {
         int rev=0;
         while(num>0){
             int rem=num%10;
-              rev=rev*10+rem;
+            rev=rev*10+rem;
             num/=10;
         }
         System.out.println(rev);
