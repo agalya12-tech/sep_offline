89a4f1296d7de5123067377d2b01d48e25bcfdcd
Excelr Solutions <mailmeasradha@gmail.com>
2025-09-25 16:08:21 +0530

    js introduction and operators

---
+++ frontend/javascript/tokens/operators.js
@@ -0,0 +1,37 @@
+value1=10;
+value2=2; 
+//arithmetic operators : + , -, *, /, % , ** 
+console.log(value1+value2);//12
+console.log(value1-value2);//8
+console.log(value1*value2);//20
+console.log(value1/value2);//5
+console.log(value1%value2);//0
+console.log(value1**value2);//100
+
+
+//relational operators
+value3=10;
+value4="10";
+console.log(value3==value4);//true
+console.log(value3===value4);//false
+
+//assignment operators
+sum=0;
+for( i=1;i<=3;i++){
+    // sum=sum+i;
+    sum+=i;
+}
+console.log(sum);
+value5=10;
+// value5=value5*2;
+value5*=2;
+
+
+
+//conditional operator
+value6=10;
+res=value6%2==0?"even number":"odd number";
+console.log(res);
+
+// console.log(value6%2==0?"even number":"odd number");
+
---
+++ frontend/javascript/tokens/task1.js
@@ -0,0 +1,7 @@
+  document.getElementById('div1').onClick = function(){
+             alert('hello world thorough  external js');
+ }
+
+ function f1(){
+    
+ }
