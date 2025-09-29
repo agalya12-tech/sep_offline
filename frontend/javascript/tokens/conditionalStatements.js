c9c63a99ca02618e618b90526ee4f237e42b2f0e
Excelr Solutions <mailmeasradha@gmail.com>
2025-09-29 14:03:37 +0530

    conditional and looping

---
+++ frontend/javascript/tokens/conditionalStatements.js
@@ -0,0 +1,31 @@
+let age=18;
+gender="female";
+// if statement
+// if(age>=18){
+//     console.log("eligible to vote");
+// }
+
+
+// else if or if else statement
+// if(age>=18){
+//     console.log("eligible to vote");
+    
+// }else{
+//     console.log("not eligible to vote");
+// }
+
+
+
+//  nested if statements
+if(age>=18){
+   if(gender=='male'){
+    console.log('enter into Q 1');
+   }
+   else{
+    console.log('enter into Q 2');
+   }
+}
+else{
+    console.log("not eligible to vote");
+}
+
