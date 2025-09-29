1897776c0962c912673a5f23f03903e3c0403958
Excelr Solutions <mailmeasradha@gmail.com>
2025-09-29 15:28:51 +0530

    conditional and looping

--- frontend/javascript/tokens/conditionalStatements.js
+++ frontend/javascript/tokens/conditionalStatements.js
@@ -1,20 +1,17 @@
-let age=18;
-let gender="female";
+let age = 18;
+let gender = "female";
 // if statement
 // if(age>=18){
 //     console.log("eligible to vote");
 // }
 
-
 // else if or if else statement
 // if(age>=18){
 //     console.log("eligible to vote");
-    
+
 // }else{
 //     console.log("not eligible to vote");
 // }
-
-
 
 //  nested if statements
 // if(age>=18){
@@ -29,8 +26,6 @@ let gender="female";
 //     console.log("not eligible to vote");
 // }
 
-
-
 //  else if ladder
 // let weekday=prompt("enter the weekday");
 
@@ -43,18 +38,33 @@ let gender="female";
 // else if(weekday=='saturday'){console.log("today is saturday...poori");}
 // else{console.log("invalid day");}
 
+let choice = prompt(
+  "enter the choice \n1.Kids Wear" +
+    " \n2.Mens Wear \n3.Womens Wear \n4.Sports Wear "
+);
 
-let choice = prompt("enter the choice \n1.Kids Wear"+ 
-   " \n2.Mens Wear \n3.Womens Wear \n4.Sports Wear ");
-
-let key = parseInt(choice);//converts string into integer
-console.log(choice , typeof choice );
-console.log(key , typeof key);
+let key = parseInt(choice); //converts string into integer
+console.log(choice, typeof choice);
+console.log(key, typeof key);
 switch (key) {
-    case 1:console.log('kids wear ....!!!');break;
-    case 2:console.log('mens wear ....!!!');break;
-    case 3: console.log('womens wear ....!!!');break;
-    case 4: console.log('sports wear ....!!!');break;
-    default:console.log('invalid choice');break;
-    
+  case 1:
+    {
+      console.log("kids wear ....!!!");
+    }
+    break;
+  case 2:
+    {
+      console.log("mens wear ....!!!");
+      console.log("mens wear ....!!!");
+    }
+    break;
+  case 3:
+    console.log("womens wear ....!!!");
+    break;
+  case 4:
+    console.log("sports wear ....!!!");
+    break;
+  default:
+    console.log("invalid choice");
+    break;
 }
