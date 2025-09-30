e4abf048c9e08358a81187677af59ee4d98ecf35
Excelr Solutions <mailmeasradha@gmail.com>
2025-09-30 14:55:55 +0530

    functions

--- frontend/javascript/function/function1.js
+++ frontend/javascript/function/function1.js
@@ -1,32 +1,28 @@
-
-
 f1(); // function hoisting - before creating a function,
-     //  we can call the function
-function f1(){ // no argumented function/non parameter function
-     for(let i=1;i<=5;i++){
-         document.write(i+" ");
-     }
-     document.write("<br>");
+//  we can call the function
+function f1() {
+  // no argumented function/non parameter function
+  for (let i = 1; i <= 5; i++) {
+    document.write(i + " ");
+  }
+  document.write("<br>");
 }
 f1();
 // -------------------------------------------------------
-
-
-function f2(value){ //argumented function/parameter function
-     for(let i=1;i<=5;i++){
-         document.write(value+" ");
-     }
-     document.write("<br>");
+function f2(value) {
+  //argumented function/parameter function
+  for (let i = 1; i <= 5; i++) {
+    document.write(value + " ");
+  }
+  document.write("<br>");
 }
 f2("*");
-f2('$');
-
-
-function table(num){
-    for(let i=1;i<=10;i++){
-        document.write(num+" * "+i+" = "+(num*i)+"<br>");
-    }
-    document.write("<br>");
+f2("$");
+function table(num) {
+  for (let i = 1; i <= 10; i++) {
+    document.write(num + " * " + i + " = " + num * i + "<br>");
+  }
+  document.write("<br>");
 }
 
 table(2);
@@ -34,3 +30,18 @@ table(3);
 table(15);
 table(19);
 
+// -------------------------------------------------------
+
+function isPrime(num) {
+  let count = 0;
+  for (let i = 1; i <= num; i++) {
+    if (num % i == 0) {
+      count++;
+    }
+  }
+  return count == 2;
+}
+// let res=isPrime(4);
+// console.log(res, typeof res);
+isPrime(3)? console.log("prime"):console.log("not prime");
+isPrime(4)? console.log("prime"):console.log("not prime");
