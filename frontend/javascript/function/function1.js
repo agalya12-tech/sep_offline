c23bd70d317e6143ef785a79a9cb366b6a4e820a
Excelr Solutions <mailmeasradha@gmail.com>
2025-09-30 14:37:52 +0530

    functions

---
+++ frontend/javascript/function/function1.js
@@ -0,0 +1,36 @@
+
+
+f1(); // function hoisting - before creating a function,
+     //  we can call the function
+function f1(){ // no argumented function/non parameter function
+     for(let i=1;i<=5;i++){
+         document.write(i+" ");
+     }
+     document.write("<br>");
+}
+f1();
+// -------------------------------------------------------
+
+
+function f2(value){ //argumented function/parameter function
+     for(let i=1;i<=5;i++){
+         document.write(value+" ");
+     }
+     document.write("<br>");
+}
+f2("*");
+f2('$');
+
+
+function table(num){
+    for(let i=1;i<=10;i++){
+        document.write(num+" * "+i+" = "+(num*i)+"<br>");
+    }
+    document.write("<br>");
+}
+
+table(2);
+table(3);
+table(15);
+table(19);
+
