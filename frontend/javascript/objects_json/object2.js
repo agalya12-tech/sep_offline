171a08270ef7ea21eb8a9fde54f879e60c46dc51
Excelr Solutions <mailmeasradha@gmail.com>
2025-10-06 15:33:58 +0530

    objects

---
+++ frontend/javascript/objects_json/object2.js
@@ -0,0 +1,63 @@
+let product= {
+      "id": 2,
+      "title": "Eyeshadow Palette with Mirror",
+      "rating": 2.86,
+      "stock": 34,
+      "tags": [ "beauty", "eyeshadow" ],
+      "brand": "Glamour Beauty",
+      "sku": "BEA-GLA-EYE-002",
+      "weight": 9,
+      "dimensions": {
+        "width": 9.26,
+        "height": 22.47,
+        "depth": 27.67
+      },
+      "thumbnail": "https://cdn.dummyjson.com/product-images/beauty/eyeshadow-palette-with-mirror/thumbnail.webp"
+    }
+
+ //  accessing object values 
+        //  print  title
+        console.log(product.title);
+        // print dimensions value
+        console.log(product.dimensions.height);
+        
+// manipulation
+        product.title="Black EyeShadow";
+        console.log(product["title"]);
+    
+        
+        console.log(product);
+
+
+
+//  creating a new object using prototype from old object
+let newProduct=Object.create(product);
+console.log(newProduct);
+console.log(newProduct.title);
+
+
+
+
+let person={
+    name:"raj",
+    age:34,
+    phone:1234567890,
+    details:function(){
+       console.log("The person name is ", this.name, " and age is ", this.age);
+    }
+}
+
+console.log(person);
+let newPerson=Object.create(person);
+console.log(newPerson);
+console.log(newPerson.name);
+newPerson.details();
+
+
+
+
+
+
+
+
+    
