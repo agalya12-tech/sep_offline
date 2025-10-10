311eb47f1c1d1502a4eed09c5d1f7491c51f6ac1
Excelr Solutions <mailmeasradha@gmail.com>
2025-10-10 15:27:54 +0530

    dom

--- frontend/dom/elementManipulation.js
+++ frontend/dom/elementManipulation.js
@@ -1,9 +1,20 @@
 let fruits = ["banana", "apple", "mango", "orange", "grapes", "kiwi"];
 
-
 let ul = document.querySelector("ul");
 for (let i = 0; i < fruits.length; i++) {
   let li = document.createElement("li");
-  li.innerHTML = fruits[i];
+  li.textContent = fruits[i];
   ul.appendChild(li);
 }
+
+
+//  adding child elements into parent element
+let parent = document.getElementById("parent");
+
+let child1 = document.createElement("div");
+child1.innerHTML = "child1";
+parent.appendChild(child1); //add at end
+
+let child2 = document.createElement("div");
+child2.innerHTML = "child2";
+parent.prepend(child2); //add at starting position
