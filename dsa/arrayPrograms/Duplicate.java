c5f63ae250df791e15417f67544038b03e92b216
Excelr Solutions <mailmeasradha@gmail.com>
2025-10-15 17:01:20 +0530

    distinct and suplicates

---
+++ dsa/arrayPrograms/Duplicate.java
@@ -0,0 +1,18 @@
+public class Duplicate {
+   public static void main(String[] args) {
+    
+        int[] arr = { 10, 20, 20};
+        for(int i=0;i<arr.length;i++){
+            int count=0;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==arr[j])
+                count++;
+            }
+            if(count==1)
+            System.out.println(arr[i]);
+            
+
+        }
+    }
+}
+
