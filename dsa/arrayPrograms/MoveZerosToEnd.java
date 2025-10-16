55ee08e4c6659ac473c2ce4f2fb29b3d85b3fd4f
Excelr Solutions <mailmeasradha@gmail.com>
2025-10-16 17:00:04 +0530

    hooks

---
+++ dsa/arrayPrograms/MoveZerosToEnd.java
@@ -0,0 +1,21 @@
+public class MoveZerosToEnd {
+    
+    public static void main(String[] args) {
+            
+         int []arr={0,0,13,12,0};
+	        int index=0;
+	        for(int i=0;i<arr.length;i++){
+	          int element=arr[i];
+	          if(element!=0){
+	            arr[index]=element;
+	            index++;
+	          }
+    	        }
+        
+            //move zeros	        
+	        for(int i=index;i<arr.length;i++){
+	          arr[i]=0;
+	        }
+
+    }
+}
