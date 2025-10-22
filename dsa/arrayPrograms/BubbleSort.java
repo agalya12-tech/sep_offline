880a2672c8ac38b0dd44b23bbfdc7b9f8ee8673b
Excelr Solutions <mailmeasradha@gmail.com>
2025-10-22 16:54:21 +0530

    controlled and uncontrolled components

---
+++ dsa/arrayPrograms/BubbleSort.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+class BubbleSort {
+    public static void main(String[] args) {
+        int []arr={4,3,1,6,7};
+        System.out.println("Unsorted array "+Arrays.toString(arr));
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println("sorted array "+Arrays.toString(arr));
+    }
+}
