0a31c6578661d4a8336a57121f8b356ce655e3c5
Excelr Solutions <mailmeasradha@gmail.com>
2025-10-22 15:11:50 +0530

    controlled and uncontrolled components

--- frontend/react/react_1/src/components/Controlled.jsx
+++ frontend/react/react_1/src/components/Controlled.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 
-const Controlled = () => { 
-    let[data,setData]=React.useState("");
-    let[msg,setMsg]=React.useState("");
-    function collectData(event){
-        setData(event.target.value);
+const Controlled = () => {
+  let [data, setData] = React.useState("");
+  let [msg,setMsg] = React.useState("weak password");
+  function collectData(event){
+    setData(event.target.value);
+    if(data.length>=8){ 
+       if('1234567890'.split('').some((num)=>data.includes(num))) {
+        setMsg("strong password");
+       }else{
+        setMsg("password should contain  at least one number");
+       }
+    }else{
+      setMsg("length should be more than 8 characters");
     }
+  }
   return (
-   <>
-   <input type="text" onChange={collectData} placeholder='enter password' />
-   <p style={{color:'red'}}>{msg}</p>
-   </>
+    <>
+    <div>Collected Data : {data}</div>
+    <input type="text"  onChange={collectData} placeholder='enter password' />
+    <p style={{color:"red"}}>{msg}</p>
+    </>
   )
-} 
+}
 
-export default Controlled 
+export default Controlled
