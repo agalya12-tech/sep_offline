e246f07d7c8ea96a85f263c6bfaf7714bd7b3d17
Excelr Solutions <mailmeasradha@gmail.com>
2025-10-24 15:48:46 +0530

    react components and events

--- frontend/react/react_1/src/events/EventExample2.jsx
+++ frontend/react/react_1/src/events/EventExample2.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 
 const EventExample2 = () => {
-      let[color,setColor]=React.useState(false);
+      let[color,setColor]=React.useState('red');
     let s1={
         border:'4px dotted black',
         width:'400px',
         height:'400px',
         margin:'20px',
-        backgroundColor:color?'green':'red'
+        backgroundColor:color
     }
     // function handleMouseEnter(){
     //     setColor(true);
     // }
     function handleMouseLeave(){ 
-        setColor(false);
+        setColor('red');
     }
     return (
         <>
          <div style={s1}
-         onMouseEnter={()=>setColor(true)}
+         onMouseEnter={()=>setColor('green')}
          onMouseLeave={handleMouseLeave}
          ></div>
 
