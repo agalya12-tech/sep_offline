82a29f857e4fd0417f6c3b365668849cbb483e90
Excelr Solutions <mailmeasradha@gmail.com>
2025-10-27 16:54:48 +0530

    spa notes

--- frontend/react/spa_1/src/App.jsx
+++ frontend/react/spa_1/src/App.jsx
@@ -3,14 +3,21 @@ import { Login } from './components/Login'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Dashboard from './components/Dashboard'
 import Error from './components/Error'
+import Users from './components/Users'
+import Products from './components/Products'
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Login />}></Route>
-          <Route path='/dashboard' element={<Dashboard/>}></Route>
           <Route path='/error' element={<Error/>}></Route>
+          <Route path='/dashboard' element={<Dashboard/>}>
+
+               <Route path='/dashboard/users' element={<Users/>}></Route>
+               <Route path='/dashboard/products' element={<Products/>}></Route>
+
+          </Route>
         </Routes>
       </BrowserRouter>
 
--- frontend/react/spa_1/src/components/Dashboard.jsx
+++ frontend/react/spa_1/src/components/Dashboard.jsx
@@ -1,37 +1,35 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 const Dashboard = () => {
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{ backgroundColor: " #e3f2fd;" }} data-bs-theme="light">
-                <div class="container-fluid">
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Welcome To My Application</a>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{ backgroundColor: " #e3f2fd" }} data-bs-theme="light">
+                <div className="container-fluid">
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Welcome To My Application</a>
                             </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Fetch
                                 </a>
-                                <ul class="dropdown-menu">
+                                <ul className="dropdown-menu">
                                     <li><Link className='dropdown-item' to={'/dashboard/users'}>Users</Link></li>
-                                    <li><a class="dropdown-item" href="#">Products</a></li>
+                                    <li><Link className="dropdown-item" to={'/dashboard/products'}>Products</Link></li>
                                 </ul>
                             </li>
 
                         </ul>
-                        <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
                         </form>
                     </div>
                 </div>
-
-
-
             </nav>
+            <Outlet></Outlet>
         </>
     )
 }
