670d26673997714c9b46978af4f226425c628d8c
Excelr Solutions <mailmeasradha@gmail.com>
2025-10-28 15:11:18 +0530

    axios notes

--- frontend/react/spa_1/src/components/Users.jsx
+++ frontend/react/spa_1/src/components/Users.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import axios from 'axios';
 const Users = () => {
 
-  function fetchUsers() {
-    // fetch('https://fakestoreapi.com/users')
-    //   .then(response => response.json())
-    //   .then(data => console.log(data));
-
-   
+    let[users,setUsers]=React.useState([]);
+  useEffect(
+    () => {
       axios.get('https://fakestoreapi.com/users')
-      .then(response => console.log(response.data));
-  }
-  fetchUsers();
-
+      .then(response => {
+        console.log(response.data);
+        setUsers(response.data)});
+    },[]
+  )
   return (
+    <>
+     <div className='container text-center'>
+      {
+      users.map((user)=><div key={user.id} className='item'>
+        <div>USERNAME: <b>{user.username}</b> </div>
+        <div>EMAIL: <b>{user.email}</b></div>
+        <div>PHONE: <b>{user.phone}</b></div>
 
-    <div>Users</div>
+      </div>)
+    }
+     </div>
+    </>
+    
   )
 }
 
