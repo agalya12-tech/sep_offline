41d96143c3ae53fa08f37f970fcdf3cf2dd146aa
Excelr Solutions <mailmeasradha@gmail.com>
2025-10-28 14:35:03 +0530

    axios notes

--- frontend/react/spa_1/src/components/Users.jsx
+++ frontend/react/spa_1/src/components/Users.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import axios from 'axios';
 const Users = () => {
 
-function fetchUsers(){
-    fetch('https://fakestoreapi.com/users')
-}
+  function fetchUsers() {
+    // fetch('https://fakestoreapi.com/users')
+    //   .then(response => response.json())
+    //   .then(data => console.log(data));
+
+   
+      axios.get('https://fakestoreapi.com/users')
+      .then(response => console.log(response.data));
+  }
+  fetchUsers();
 
   return (
 
