5f2aaed7b0cfc8bf49593c017cbbf4d2d0f3ced9
Excelr Solutions <mailmeasradha@gmail.com>
2025-10-29 15:52:46 +0530

    axios notes

--- frontend/react/spa_1/src/components/EditProduct.jsx
+++ frontend/react/spa_1/src/components/EditProduct.jsx
@@ -1,25 +1,38 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { createRef, useEffect, useRef, useState } from 'react'
 import { useLocation, useParams, useSearchParams } from 'react-router-dom'
 
 export const EditProduct = () => {
 
-  
+
   //   let {id}=  useParams(); -- path parameter
-  
+
   let [id] = useSearchParams(); // query parameter
   let pid = id.get('id');
-  
-  
+
+
   //  let location=useLocation();
   //  console.log(location);
-  
-  let [product,setProduct]=useState({});
-  let[editProduct,setEditProduct]=useState({});
 
-   function handleChange(e){
-     setEditProduct({...editProduct,[e.target.name]:e.target.value});
-   }
+  let [product, setProduct] = useState({});
+  let title = useRef(null);
+  let description = useRef(null);
+  let category = useRef(null);
+  let price = useRef(0);
+  function handleEdit(e) {
+    e.preventDefault();
+    let product = {
+      id: pid,
+      title: title.current.value,
+      description: description.current.value,
+      category: category.current.value,
+      price: price.current.value
+    }
+    console.log(product);
+
+  }
+
+
 
 
 
@@ -29,10 +42,12 @@ export const EditProduct = () => {
     //   .then(data => console.log(data));
     axios.get(`https://fakestoreapi.com/products/${pid}`)
       .then(response => {
-        console.log(response.data)
+        // console.log(response.data)
         setProduct(response.data);
       });
   }, []);
+
+
 
 
 
@@ -46,22 +61,22 @@ export const EditProduct = () => {
 
     <>
       <label htmlFor="id">ID :  </label>
-      <input type="text" id='id' defaultValue={product.id} readOnly onChange={handleChange} /> <br /><br />
-    
+      <input type="text" id='id' defaultValue={product.id} readOnly /> <br /><br />
+
       <label htmlFor="title">Title :  </label>
-      <input type="text" id='title' defaultValue={product.title} onChange={handleChange} /> <br /><br />
-    
+      <input type="text" id='title' defaultValue={product.title} ref={title} /> <br /><br />
+
       <label htmlFor="desc">Description :  </label>
-      <input type="text" id='desc' defaultValue={product.description} onChange={handleChange} /> <br /><br />
-    
+      <input type="text" id='desc' defaultValue={product.description} ref={description} /> <br /><br />
+
       <label htmlFor="category">Category :  </label>
-      <input type="text" id='category' defaultValue={product.category}  onChange={handleChange} /> <br /><br />
-    
+      <input type="text" id='category' defaultValue={product.category} ref={category} /> <br /><br />
+
       <label htmlFor="price">Price :  </label>
-      <input type="text" id='price' defaultValue={product.price} onChange={handleChange} /> <br /><br />
-    
-       <button className='btn btn-warning '>Update</button>
-     
+      <input type="text" id='price' defaultValue={product.price} ref={price} /> <br /><br />
+
+      <button className='btn btn-warning ' onClick={handleEdit}>Update</button>
+
     </>
   )
 }
