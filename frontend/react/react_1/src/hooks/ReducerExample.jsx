cf33b0b6a0c69dab9ff84bc2f2cf0dffbac51127
Excelr Solutions <mailmeasradha@gmail.com>
2025-10-30 15:32:11 +0530

    reducer notes

--- frontend/react/react_1/src/hooks/ReducerExample.jsx
+++ frontend/react/react_1/src/hooks/ReducerExample.jsx
@@ -25,17 +25,26 @@ const ReducerExample = () => {
    function reducer(state, action) {
     switch(action.type){
         case "INCREMENT": return {count:state.count+1};
+        case "INCREMENTBY5": return {count:state.count+5};
         case "DECREMENT":return {count:state.count-1};
         default: return state;
     }}
    let [state, dispatch]=useReducer(reducer, {count:0});
    function increment() {dispatch({type:"INCREMENT"});}
+   function incrementBy5() {dispatch({type:"INCREMENTBY5"});}
    function decrement() {dispatch({type:"DECREMENT"});}
+   function decrementBy2s() {
+    setTimeout(() => {
+        dispatch({type:"DECREMENT"});
+    }, 2000);
+   }
   return (
     <>
-     <button className='btn btn-success' onClick={increment}>+</button>
-     {state.count}
+     {state.count} <br/>
+     <button className='btn btn-danger' onClick={decrementBy2s}>Decrement By 2s</button>
      <button className='btn btn-danger' onClick={decrement}>-</button>
+     <button className='btn btn-success' onClick={increment}>+</button>
+     <button className='btn btn-success' onClick={incrementBy5}>Increment By 5</button>
     </>
   )}
 export default ReducerExample
