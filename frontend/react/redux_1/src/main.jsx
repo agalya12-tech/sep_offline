a6204915d8d783cff4c1831d2e8cd89d849e55b6
Excelr Solutions <mailmeasradha@gmail.com>
2025-10-31 14:10:00 +0530

    redux  notes

--- frontend/react/redux_1/src/main.jsx
+++ frontend/react/redux_1/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Component1 from './components/Component1.jsx'
+import Component2 from './components/Component2.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Component1/>
+    <Component2/>
   </StrictMode>,
 )
