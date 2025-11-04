dccdcc6dd2eed666584f859c456a7a3803e58630
Excelr Solutions <mailmeasradha@gmail.com>
2025-11-04 15:13:25 +0530

    mern

--- projects/mernProject/userFrontend/src/components/Register.jsx
+++ projects/mernProject/userFrontend/src/components/Register.jsx
@@ -1,9 +1,27 @@
+import axios from 'axios';
 import React from 'react'
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+    const [image, setImage] = React.useState(null);
 
+     function handleSubmit(e){
+        e.preventDefault();
+       
+        let formData= new FormData();
+        formData.append('file', image);
+        formData.append('upload_preset', 'mern_project');
+        formData.append('cloudinary', 'dsbwkapof');
+        axios.post('https://api.cloudinary.com/v1_1/dsbwkapof/image/upload', formData)
+       
+     }
+
+    return (
+        <>
+        <input type="file" accept="image/*" 
+        onChange={(e)=>setImage(e.target.files[0])}
+         /> <br /><br />
+        <button onClick={handleSubmit}>Upload</button>
+        </>
+    )
+}
 export default Register
