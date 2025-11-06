e5236474012805b2a565398e51680717f171a1c4
Excelr Solutions <mailmeasradha@gmail.com>
2025-11-06 15:51:29 +0530

    java notes

---
+++ backend/basic_project/src/basic_project/DataTypeProgram1.java
@@ -0,0 +1,18 @@
+package basic_project;
+/*
+ * Primitive Data types:
+ * byte=Byte , short=Short , int=Integer , long=Long
+ * double=Double , float=Float , char=Character
+ * */
+public class DataTypeProgram1 {
+
+	public static void main(String[] args) {
+		  byte minByte=Byte.MIN_VALUE;
+		  byte maxByte=Byte.MAX_VALUE;
+		  System.out.println(minByte+"  "+maxByte);
+		  
+		  short minShort=Short.MIN_VALUE;
+		  short maxShort=Short.MAX_VALUE;
+		  System.out.println(minShort+"  "+maxShort);
+	}
+}
---
+++ backend/basic_project/src/basic_project/FirstProgram.java
@@ -0,0 +1,9 @@
+package basic_project;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+    System.out.println("hello world ...!!!");
+	}
+
+}
