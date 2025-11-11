df5d576f440cb73ac3dc35b5b9ddc89fb0888f4b
Excelr Solutions <mailmeasradha@gmail.com>
2025-11-11 17:08:50 +0530

    method and class notes

--- backend/basic_project/src/methods/Program1.java
+++ backend/basic_project/src/methods/Program1.java
@@ -19,23 +19,49 @@ public class Program1 {
 	
 	
 	
+//	
+//	public static void m1() {
+////		 System.out.println(instanceVariable);
+//		 System.out.println(staticVariable);
+//		 staticMethod();
+////		 instanceMethod();
+//		 Program1 program1=new Program1();
+//		 System.out.println(program1.instanceVariable);
+//		 program1.instanceMethod();
+//	}
+//	public void m2() {
+//		System.out.println(instanceVariable);
+//		System.out.println(staticVariable);
+//		staticMethod();
+//		instanceMethod();
+//	}
 	
-	public static void m1() {
-//		 System.out.println(instanceVariable);
+	public static void main(String[] args) {
 		 System.out.println(staticVariable);
 		 staticMethod();
-//		 instanceMethod();
+		 System.out.println(staticVariable);
+		 staticMethod();
+		 
+		 Program1 program=new Program1();
+		 System.out.println(program.instanceVariable);
+		 program.instanceMethod();
 		 Program1 program1=new Program1();
 		 System.out.println(program1.instanceVariable);
 		 program1.instanceMethod();
+		 
+		 /**
+		  * Expected O/p -- null staticMethod null staticMethod 0 instanceMethod 0 instanceMethod
+		  * 
+		  * Actual o/p--static initlizers  
+		  *                 null staticMethod null staticMethod
+		  *             non static initlizers
+		  *                 0 instanceMethod
+		  *             non static initlizers
+		  *                 0 instanceMethod
+		  *                 
+		  */	 
+		 
 	}
-	public void m2() {
-		System.out.println(instanceVariable);
-		System.out.println(staticVariable);
-		staticMethod();
-		instanceMethod();
-	}
-	
 	
 	
 
