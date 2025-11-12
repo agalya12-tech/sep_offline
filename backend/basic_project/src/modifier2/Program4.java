4b7e5c97e0df0aa03886f524145d994fc8b8393e
Excelr Solutions <mailmeasradha@gmail.com>
2025-11-12 17:07:46 +0530

    Constrcutor  , Constrcutor overloading and Access Modifiers

--- backend/basic_project/src/modifier2/Program4.java
+++ backend/basic_project/src/modifier2/Program4.java
@@ -5,7 +5,9 @@ import modifier1.Program1;
 public class Program4  extends Program1{
 	public static void main(String[] args) {
 		Program1 program1=new Program1();
+		Program4 program4=new Program4();
 		System.out.println(program1.publicVariable);
+		System.out.println(program4.protectedVariable);
 //		System.out.println(program1.protectedVariable);
 //		System.out.println(program1.defaultVariable);
 //		System.out.println(program1.privateVariable);
