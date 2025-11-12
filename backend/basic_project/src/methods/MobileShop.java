d700c5bf88771e19a3b293bffed7182c65956647
Excelr Solutions <mailmeasradha@gmail.com>
2025-11-12 14:42:09 +0530

    Constrcutor  , Constrcutor overloading and Access Modifiers

--- backend/basic_project/src/methods/MobileShop.java
+++ backend/basic_project/src/methods/MobileShop.java
@@ -2,10 +2,15 @@ package methods;
 
 public class MobileShop {
 	public static void main(String[] args) {
-     Mobile mobile=new Mobile();
-     System.out.println(mobile);
+     Mobile m=new Mobile();
+     System.out.println(m+" before assigning");
+     m.storage=16;
+     m.brand="Vivo";
+     System.out.println(m+" after assigning");
+     System.out.println("=================================================");
+     System.out.println();
+     System.out.println("==================================================");
      
-     Mobile mobile1=new Mobile();
-     System.out.println(mobile1);
+    
 	}
 }
