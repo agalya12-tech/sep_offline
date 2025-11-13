1ca6078a110568bb71ed94a5b779e13eebbc7abc
Excelr Solutions <mailmeasradha@gmail.com>
2025-11-13 15:50:38 +0530

    array  notes

--- backend/basic_project/src/arrayManipulation/Array4.java
+++ backend/basic_project/src/arrayManipulation/Array4.java
@@ -6,13 +6,23 @@ import java.util.Collections;
 public class Array4 {
 
 	public static void main(String[] args) {
-		Integer []arr= {2,5,1,6,7};
+		Integer[] arr = { 2, 5, 1, 6, 7 };
 		System.out.println(Arrays.toString(arr));
-		
+
 //		Arrays.sort(arr); default ascending order
 //		System.out.println(Arrays.toString(arr));
-		Arrays.sort(arr,Collections.reverseOrder());// descending order
+
+		Arrays.sort(arr, Collections.reverseOrder());// descending order
 		System.out.println(Arrays.toString(arr));
+
+		int[] arr2 = { 1, 2, 3 };
+		int[] arr3 = { 1, 2, 5 };
+
+		System.out.println(arr2 + " " + arr3);
+//	will compare two array values , if same it returns true or else false
+		System.out.println(Arrays.equals(arr2, arr3));
+		
+		System.out.println(Arrays.mismatch(arr2, arr3));
 
 	}
 }
