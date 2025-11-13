240c0799fe5a1eafcba38ff624d0667c2c1e8012
Excelr Solutions <mailmeasradha@gmail.com>
2025-11-13 15:56:43 +0530

    array  notes

--- backend/basic_project/src/arrayManipulation/Array4.java
+++ backend/basic_project/src/arrayManipulation/Array4.java
@@ -16,13 +16,20 @@ public class Array4 {
 		System.out.println(Arrays.toString(arr));
 
 		int[] arr2 = { 1, 2, 3 };
-		int[] arr3 = { 1, 2, 5 };
+		int[] arr3 = { 1, 2, 3 };
 
 		System.out.println(arr2 + " " + arr3);
 //	will compare two array values , if same it returns true or else false
 		System.out.println(Arrays.equals(arr2, arr3));
-		
+//	will compare two array and finds first mismatch element , if found 
+//	it returns its index position or else returns -1
 		System.out.println(Arrays.mismatch(arr2, arr3));
 
+		if(Arrays.mismatch(arr2, arr3)==-1) {
+			System.out.println("both arrays elements are same");
+		}else {
+			System.out.println("both arrays elements are not same");
+		}
+		
 	}
 }
