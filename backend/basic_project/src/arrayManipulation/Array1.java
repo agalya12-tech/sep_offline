d49ee3a1f45a07614e727c6b29ff058cc50dc309
Excelr Solutions <mailmeasradha@gmail.com>
2025-11-13 14:28:25 +0530

    array  notes

---
+++ backend/basic_project/src/arrayManipulation/Array1.java
@@ -0,0 +1,15 @@
+package arrayManipulation;
+
+import methods.Cat;
+
+public class Array1 {
+// static  int[]arr2; // declaration for global
+	public static void main(String[] args) {
+//		int [] arr;//declaration for local
+//		arr= {10,20,30}; //inilization for local
+		int[]arr= {10,20,30};//declaration and initlization
+		int[]arr1=new int[9];//declaration
+		int[]arr2=new int[]{10,20,30,40};//declaration and initlization
+//		arr2= {10,20,30};inilization for local
+	}
+}
---
+++ backend/basic_project/src/arrayManipulation/Array2.java
@@ -0,0 +1,8 @@
+package arrayManipulation;
+
+public class Array2 {
+
+	public static void main(String[] args) {
+		
+	}
+}
