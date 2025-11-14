99a545228e25731567a657c7e0e0c73baab01a36
Excelr Solutions <mailmeasradha@gmail.com>
2025-11-14 15:18:32 +0530

    array and String  notes

---
+++ backend/basic_project/src/arrayManipulation/Array8.java
@@ -0,0 +1,10 @@
+package arrayManipulation;
+
+public class Array8 {
+
+	public static void main(String[] args) {
+		int[][] arr = { { 10, 20, 30 }, { 40, 50, 60 }, { 70, 80 ,90} };
+		int[][] arr2 = { { 10, 20, 30 }, { 40, 50, 60 }, { 70, 80 ,90} };
+
+	}
+}
