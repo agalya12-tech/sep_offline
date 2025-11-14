4767e32d5c58a2db08db76314004d927d73ce6b0
Excelr Solutions <mailmeasradha@gmail.com>
2025-11-14 14:46:57 +0530

    array and String  notes

--- backend/basic_project/src/arrayManipulation/Array6.java
+++ backend/basic_project/src/arrayManipulation/Array6.java
@@ -16,7 +16,21 @@ public class Array6 {
 		System.out.println(arr[2][2]);//NINE
 		System.out.println(arr[0][2]);//THREE
 		arr[2][1]="TEN";
+		
+//		for printing multidimensional array in single line
 		System.out.println(Arrays.deepToString(arr));
+//		for printing array present in multidimensional array based on index position
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(Arrays.toString(arr[i])+" "+i +" position");
+		}
+//	for printing each value in MDA 
+		System.out.println("=====Accessing Values=========");
+		for(int row=0;row<arr.length;row++) {
+			for(int col=0;col<arr[row].length;col++) {
+				System.out.println(arr[row][col]);
+			}
+			System.out.println("-------------");
+		}
 		
 	}
 }
