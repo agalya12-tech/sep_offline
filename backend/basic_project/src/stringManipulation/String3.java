c171276b26a76db3f4969ce094d5d618e2e4a4ab
Excelr Solutions <mailmeasradha@gmail.com>
2025-11-14 16:47:10 +0530

    array and String  notes

--- backend/basic_project/src/stringManipulation/String3.java
+++ backend/basic_project/src/stringManipulation/String3.java
@@ -1,5 +1,20 @@
 package stringManipulation;
 
-public class String3 {
+import java.util.Arrays;
 
+public class String3 {
+	public static void main(String[] args) {
+
+		String s1="Hello";
+		String s2="hello";
+		
+		System.out.println(s1.equals(s2));//false
+		System.out.println(s1.equalsIgnoreCase(s2));//trues
+		System.out.println(s1.length());//5
+		System.out.println(s1.charAt(2));//l
+		char []arr=s1.toCharArray();
+		System.out.println(Arrays.toString(arr));//[H, e, l, l, o]
+		System.out.println(s1.toUpperCase());//HELLO
+		System.out.println(s1.toLowerCase());//hello
+	}
 }
