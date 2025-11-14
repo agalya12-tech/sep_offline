cfc48de8f0d6b4554ee338531cf162e56c1bf40b
Excelr Solutions <mailmeasradha@gmail.com>
2025-11-14 15:32:31 +0530

    array and String  notes

--- backend/basic_project/src/arrayManipulation/Array8.java
+++ backend/basic_project/src/arrayManipulation/Array8.java
@@ -1,10 +1,28 @@
 package arrayManipulation;
 
+import java.util.Arrays;
+
 public class Array8 {
 
 	public static void main(String[] args) {
-		int[][] arr = { { 10, 20, 30 }, { 40, 50, 60 }, { 70, 80 ,90} };
-		int[][] arr2 = { { 10, 20, 30 }, { 40, 50, 60 }, { 70, 80 ,90} };
+		int[][] arr1 = { { 10, 20, 30 }, 
+				         { 40, 50, 60 }, 
+				         { 70, 80 ,90} };
+		
+		int[][] arr2 = { { 100, 200, 300 },
+				         { 400, 500, 600 }, 
+				         { 700, 800 ,900} };
+		
+		int[][]output= new int[3][3];
+		
+		for(int i=0;i<arr1.length;i++) {
+			for(int j=0;j<arr1[i].length;j++) {
+			output[i][j]=arr1[i][j]+arr2[i][j];
+			}
+		}
+		System.out.println(Arrays.deepToString(arr1));
+		System.out.println(Arrays.deepToString(arr2));
+		System.out.println(Arrays.deepToString(output));
 
 	}
 }
