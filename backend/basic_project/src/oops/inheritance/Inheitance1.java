73a0b783c6a1d8235d5e3a051d165f8636b9341b
Excelr Solutions <mailmeasradha@gmail.com>
2025-11-19 14:31:50 +0530

    oops  notes

---
+++ backend/basic_project/src/oops/inheritance/Inheitance1.java
@@ -0,0 +1,15 @@
+package oops.inheritance;
+
+public class Inheitance1 extends One {
+	public static void main(String[] args) {
+       Inheitance1 inheitance1=new Inheitance1();
+//       inheitance1.privateVariable;
+	}
+}
+
+class One { //parent 
+	private String privateVariable = "private Member";
+
+}
+
+
--- backend/basic_project/src/oops/inheritance/SingleLevelInheitance.java
+++ backend/basic_project/src/oops/inheritance/SingleLevelInheitance.java
@@ -4,13 +4,15 @@ public class SingleLevelInheitance {
 		System.out.println("-----parent class------");
       Parent p=new  Parent();
       System.out.println(p.parentValue);
+//      System.out.println(p.childValue); it is present in child class
       System.out.println("----child class----");
       Child c=new Child();
       System.out.println(c.parentValue);
+      System.out.println(c.childValue);
 	}}
 class Parent{
 	String parentValue="parentValue";
 }
 class Child extends Parent{
-	
+	String childValue="Child value member";
 }
