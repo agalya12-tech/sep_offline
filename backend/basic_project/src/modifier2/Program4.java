fb2a92a9e1f23938342c82b2bf8e8e0620b5f490
Excelr Solutions <mailmeasradha@gmail.com>
2025-11-19 15:12:09 +0530

    oops  notes

--- backend/basic_project/src/modifier2/Program4.java
+++ backend/basic_project/src/modifier2/Program4.java
@@ -5,8 +5,8 @@ import modifier1.Program1;
 public class Program4  extends Program1{
 	public static void main(String[] args) {
 		Program1 program1=new Program1();
-		Program4 program4=new Program4();
 		System.out.println(program1.publicVariable);
+		Program4 program4=new Program4();
 		System.out.println(program4.protectedVariable);
 //		System.out.println(program1.protectedVariable);v
 //		System.out.println(program1.defaultVariable);
---
+++ backend/basic_project/src/oops/inheritance/Inheritance2.java
@@ -0,0 +1,25 @@
+package oops.inheritance;
+
+public class Inheritance2 {
+	public static void main(String[] args) {
+		Animal animal=new Animal();
+		animal.eats();//Animal is eating
+		
+		Dog dog=new Dog();
+		dog.eats();//Dog eats meat...
+	}
+
+}
+class Animal{
+	 public void eats() {
+		 System.out.println("animal is eating ....");
+	 }
+}
+class Dog extends Animal{
+	@Override
+	public void eats() {
+		// TODO Auto-generated method stub
+		super.eats();
+		System.out.println("Dog eats meat ...");
+	}
+}
