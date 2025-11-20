a319460859b85f6efcc065793c49c9b747d7e350
Excelr Solutions <mailmeasradha@gmail.com>
2025-11-20 15:31:21 +0530

    oops  notes

---
+++ backend/basic_project/src/oops/abstraction/Abstraction2.java
@@ -0,0 +1,47 @@
+package oops.abstraction;
+
+public class Abstraction2 {
+
+}
+
+
+class Demo1{
+	 static int a;
+	 double d;
+	 public void m1() {
+		 
+	 }
+	 public static void m2() {
+		 
+	 }
+	public Demo1(double d) {
+		super();
+		this.d = d;
+	}
+	 
+//	public abstract void m3(); // not allowed
+}
+
+
+abstract class Demo2{
+	 static int a;
+	 double d;
+	 public void m1() {
+		 
+	 }
+	 public static void m2() {
+		 
+	 }
+	public Demo2(double d) {
+		super();
+		this.d = d;
+	}
+	 
+	public abstract void m3(); 
+}
+
+
+
+
+
+
