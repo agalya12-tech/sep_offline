07b92f58e7f3be34de0fbeca67b9956429211f0e
Excelr Solutions <mailmeasradha@gmail.com>
2025-11-26 14:22:24 +0530

    wrapper and Thread notes

---
+++ backend/basic_project/src/wrapper_casting/Boxing.java
@@ -0,0 +1,14 @@
+package wrapper_casting;
+// primitive data type into non primitive data type -- Boxing
+public class Boxing {
+ public static void main(String[] args) {
+	 byte byteValue=-127;
+	 short shortValue=234;
+	 int intValue=10;
+	 
+	
+	 Byte b=byteValue;
+	
+	
+}
+}
