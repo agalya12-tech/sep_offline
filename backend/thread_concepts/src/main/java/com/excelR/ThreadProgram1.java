a272c1e25b068b08cd3af60ebbd775cf072a6ca9
Excelr Solutions <mailmeasradha@gmail.com>
2025-11-26 16:42:18 +0530

    wrapper and Thread notes

--- backend/thread_concepts/src/main/java/com/excelR/ThreadProgram1.java
+++ backend/thread_concepts/src/main/java/com/excelR/ThreadProgram1.java
@@ -1,7 +1,7 @@
 package com.excelR;
 
 public class ThreadProgram1  {
-    
+
     public static void main(String[] args) {
         Runnable r = () -> {
             System.out.println("lambda expression which represents run method inside runnable interface");
---
+++ dsa/stringPrograms/RemoveCharacter.java
@@ -0,0 +1,42 @@
+public class RemoveCharacter {
+    public static void main(String[] args) {
+        String str = "Hello World 123!";
+        System.out.println("Original String: " + str);
+        System.out.println("After removing vowels: " + removeVowels(str));
+        System.out.println("After removing consonants: " + removeConsonants(str));
+        System.out.println("After removing digits: " + removeDigits(str));
+    }
+
+    public static String removeVowels(String s) {
+        StringBuilder b = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if ("aeiouAEIOU".indexOf(c) != -1) {
+                continue;
+            }
+            b.append(c);
+        }
+        return b.toString();
+    }
+
+    public static String removeConsonants(String s) {
+        StringBuilder b = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if ("aeiouAEIOU ".indexOf(c) == -1) {
+                continue;
+            }
+            b.append(c);
+        }
+        return b.toString();
+    }
+
+    public static String removeDigits(String s) {
+        StringBuilder b = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                continue;
+            }
+            b.append(c);
+        }
+        return b.toString();
+    }
+}
