1aca199ef4587b84a474a0fd296594363dd8b85c
Excelr Solutions <mailmeasradha@gmail.com>
2025-11-26 16:15:30 +0530

    wrapper and Thread notes

---
+++ backend/thread_concepts/src/main/java/com/excelR/ThreadProgram1.java
@@ -0,0 +1,13 @@
+package com.excelR;
+
+public class ThreadProgram1  {
+    
+    public static void main(String[] args) {
+        Runnable r = () -> {
+            System.out.println("lambda expression which represents run method inside runnable interface");
+        };
+        Thread t=new Thread(r);
+        System.out.println(t);
+        t.start();
+    }
+}
