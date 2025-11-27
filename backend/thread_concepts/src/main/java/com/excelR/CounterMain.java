5a1e25d18051667584680533695a878b843dffb0
Excelr Solutions <mailmeasradha@gmail.com>
2025-11-27 15:33:17 +0530

    thread concepts

--- backend/thread_concepts/src/main/java/com/excelR/CounterMain.java
+++ backend/thread_concepts/src/main/java/com/excelR/CounterMain.java
@@ -1,15 +1,23 @@
 package com.excelR;
 
 public class CounterMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Counter counter=new Counter();
         Thread thread=new Thread(()->{
-            for(int i=1;i<=10;i++){
+            for(int i=1;i<=1000;i++){
                counter.increment();
             }
-                System.out.println(counter.getCount()+" is count");
+        });
+        Thread thread2=new Thread(()->{
+            for(int i=1;i<=1000;i++){
+               counter.increment();
+            }
         });
         thread.start();
+        thread2.start();
+        thread.join();
+        thread2.join();
+        System.out.println(counter.getCount()+" is count");
     }
 }
 
@@ -21,7 +29,7 @@ public class CounterMain {
 class Counter{
     private int count;
 
-    public void increment(){
+    public synchronized void increment(){
         this.count++;
     }
     public int getCount(){
