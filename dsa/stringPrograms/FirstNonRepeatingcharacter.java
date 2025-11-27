604b0278df35f9e1c1c7de88feae599106076e7b
Excelr Solutions <mailmeasradha@gmail.com>
2025-11-27 16:56:50 +0530

    thread concepts

---
+++ dsa/stringPrograms/FirstNonRepeatingcharacter.java
@@ -0,0 +1,12 @@
+public class FirstNonRepeatingcharacter {
+     public static void main(String[] args) {
+        String s = "swiss";
+        for (char c : s.toCharArray()) {
+            if (s.indexOf(c) == s.lastIndexOf(c)) {
+                System.out.println(c+" is the first non repeating character");
+            break;
+            }
+        }
+
+    }
+}
