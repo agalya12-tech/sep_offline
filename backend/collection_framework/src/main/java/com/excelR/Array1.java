b0d62d2ab8fb101304aa079cae586267faa57d38
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-01 15:36:00 +0530

    thread concepts notes

---
+++ backend/collection_framework/src/main/java/com/excelR/Array1.java
@@ -0,0 +1,18 @@
+package com.excelR;
+
+import java.util.Arrays;
+public class Array1 {
+    public static void main(String[] args) {
+        int[]arr={10,20,30};
+        System.out.println(Arrays.toString(arr));
+        arr[0]=100;
+        System.out.println(Arrays.toString(arr));
+        arr[4]=200;// AIOBE -- size is fixed(immutable)
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
+/*
+* Collection Framework -- is a collection of classes and interfaces
+*                      -- non -primitive data type(Wrapper class)
+* */
--- backend/thread_concepts/src/main/java/com/excelR/Waiter_Notify_Program.java
+++ backend/thread_concepts/src/main/java/com/excelR/Waiter_Notify_Program.java
@@ -14,13 +14,26 @@ public class Waiter_Notify_Program {
                 }
             }
         });
+        Thread waiterThread1=new Thread(()->{
+            synchronized (lock){
+                System.out.println("Waiting the thread to be executed");
+                try {
+                    lock.wait();
+                    System.out.println("waiting is completed by notifying");
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
         Thread notifyThread=new Thread(()->{
             synchronized (lock){
                 System.out.println("Notifying the thread to be executed");
-                lock.notify();
+                lock.notifyAll();
             }
         });
         waiterThread.start();
+        waiterThread1.start();
         notifyThread.start();
     }
 }
