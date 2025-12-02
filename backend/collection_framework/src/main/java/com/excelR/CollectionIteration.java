9efed1b5a82c6df156843ccd59538c8c9b824f50
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-02 15:35:08 +0530

    collection framework  notes

--- backend/collection_framework/src/main/java/com/excelR/CollectionIteration.java
+++ backend/collection_framework/src/main/java/com/excelR/CollectionIteration.java
@@ -3,6 +3,7 @@ package com.excelR;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -10,23 +11,42 @@ public class CollectionIteration {
     public static void main(String[] args) {
         Collection<Integer> c4=new ArrayList<>(Arrays.asList(10, 11, 12, 13,100, 200, 300, 400, 500));
         System.out.println(c4);//[10, 11, 12, 13, 100, 200, 300, 400, 500]
-//    for each loop / enhanced for loop/ advanced for loop
-        for(int i:c4){
-           System.out.print(i+" , ");
+//        c4.forEach(     //ConcurrentModification Exception
+//                (e)->{
+//                    if(e%4==0){
+//                       c4.remove(e);
+//                    }
+//                }
+//        );
+        Iterator<Integer> itr = c4.iterator();
+        while(itr.hasNext()){
+            int e=itr.next();
+            if(e%4==0){
+                itr.remove();
+            }
         }
-//   void accept(T t)
-        Consumer<Integer> consumer=(num)->{
-             System.out.println(num);
-        };
-        c4.forEach(consumer);
+        System.out.println(c4);
 
         
+
+
+//    for each loop / enhanced for loop/ advanced for loop
+//        for(int i:c4){
+//           System.out.print(i+" , ");
+//        }
+//   void accept(T t)
+//        Consumer<Integer> consumer=(num)->{
+//             System.out.println(num);
+//        };
+//        c4.forEach(consumer);
+
+
 //        boolean test(T t)
-        Predicate<Integer>p=(x)->{
-             return x%2!=0;
-        };
-        c4.removeIf(p);
-        System.out.println(c4);
+//        Predicate<Integer>p=(x)->{
+//             return x%2!=0;
+//        };
+//        c4.removeIf(p);
+//        System.out.println(c4);
 
 
 
