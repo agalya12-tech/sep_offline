7d525ef967b6982bde513ef99c3d8d723dfeabe4
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-03 14:19:07 +0530

    list notes

--- backend/collection_framework/src/main/java/com/excelR/ListProgram/ListProgram1.java
+++ backend/collection_framework/src/main/java/com/excelR/ListProgram/ListProgram1.java
@@ -5,18 +5,23 @@ import java.util.List;
 
 public class ListProgram1 {
     public static void main(String[] args) {
-        List<String>list=new ArrayList<String>();
+        List<String> list = new ArrayList<String>();
 //        list.add(2,"Orange"); //IOBE Reason size is 0
         list.add("Yellow"); // [Yellow]
-        list.add(1,"Red"); //[Yellow,Red]
-        list.add(0,"Blue");//[Blue,Yellow, Red]
+        list.add(1, "Red"); //[Yellow,Red]
+        list.add(0, "Blue");//[Blue,Yellow, Red]
         list.addFirst("Green");//[Green ,Blue,Yellow, Red]
         list.addLast("Pink");//[Green ,Blue,Yellow, Red, Pink]
+//      list.addAll(list2);
+//      list.addAll(0,list1);
 
         list.remove(0);
         list.removeFirst();
         list.removeLast();
-
+        list.remove("Yellow");
+//      list.removeAll(list1);
+//      list.retainAll(list1);
+//      list.removeIf()
         System.out.println(list);
     }
 }
