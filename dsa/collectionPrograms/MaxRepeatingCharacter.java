2eee40efc20640d1450e24c90a56cb2cf2d00e26
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-04 16:47:06 +0530

    map notes

--- dsa/collectionPrograms/MaxRepeatingCharacter.java
+++ dsa/collectionPrograms/MaxRepeatingCharacter.java
@@ -1,5 +1,26 @@
 package dsa.collectionPrograms;
 
-public class MaxRepeatingCharacter {
-    
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MaxRepeatingCharacter {	
+    public static void main(String[] args) {
+		String s="mmalayalamm";
+		Map<Character,Integer> map=new LinkedHashMap<>();
+		for(char c:s.toCharArray()) {
+			map.put(c,map.getOrDefault(c,0)+1);
+		}
+		int max=0; 
+		for(Map.Entry<Character,Integer>entry:map.entrySet()) {
+			if(entry.getValue()>max) {
+			    max=entry.getValue();
+			}
+		}
+		for(Map.Entry<Character,Integer>entry:map.entrySet()) {
+			if(entry.getValue()==max) {
+			   System.out.println(entry.getKey()+" - "+entry.getValue());
+			}
+		}
+		
+	}
 }
