f4746bc2777048aca5bb8c5fd855fd6e9a15d3f4
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-04 16:07:22 +0530

    map notes

---
+++ backend/collection_framework/src/main/java/com/excelR/mapPrograms/MapIteration.java
@@ -0,0 +1,33 @@
+package com.excelR.mapPrograms;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MapIteration {
+    public static void main(String[] args) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=1;i<=10;i++){
+             map.put(i,i+100);
+        }
+        System.out.println(map);
+//         iterate keys
+        Set<Integer> keys = map.keySet();
+        System.out.println(keys);
+//         iterate values
+        Collection<Integer> values = map.values();
+        System.out.println(values);
+
+
+//         iterate key-value pair
+        Set<Map.Entry<Integer, Integer>> pairs = map.entrySet();
+        System.out.println(pairs);
+        for(Map.Entry<Integer, Integer> entry:pairs){
+            System.out.println(entry.getKey()+" - "+entry.getValue());
+        }
+
+
+
+    }
+}
--- backend/collection_framework/src/main/java/com/excelR/mapPrograms/MapProgram3.java
+++ backend/collection_framework/src/main/java/com/excelR/mapPrograms/MapProgram3.java
@@ -7,38 +7,20 @@ import java.util.TreeMap;
 
 public class MapProgram3 {
     public static void main(String[] args) {
+        Map<Integer,String> map=new HashMap<>();
+        map.put(1,"one");
+        map.put(2,"two");
+        map.putIfAbsent(2,"Three");
+        map.putIfAbsent(3,"Three");
+        System.out.println(map); //{1=one, 2=Three, 3=Three}
+        map.replace(3,"Five");
+        map.replace(6,"Five");
+        System.out.println(map);//{1=one, 2=Three, 3=Five}
+        map.remove(13);
+//        map.remove(3,"Five");
+        System.out.println(map);//{1=one, 2=Three, 3=Five}
 
-        String s="malayalam";
-        Map<Character,Integer> map=new LinkedHashMap<>();
-        for(char c:s.toCharArray()){
-           map.put(c,map.getOrDefault(c,0)+1);
-        }
-        System.out.println(map);
-
-
-
-//        Map<Integer,String> map=new HashMap<>();
-
-
-
-
-
-
-
-
-//        map.put(1,"one");
-//        map.put(2,"two");
-//        map.putIfAbsent(2,"Three");
-//        map.putIfAbsent(3,"Three");
-//        System.out.println(map); //{1=one, 2=Three, 3=Three}
     }
 }
 
 
-/*
-* malayalam
-* m-2
-* a-4
-* l-2
-* y-1
-* */
