c5eb15dded9a08f5f89d50dcb7326389a8c05c09
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-04 15:25:10 +0530

    map notes

--- dsa/collectionPrograms/FrequencyOfWord.java
+++ dsa/collectionPrograms/FrequencyOfWord.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class FrequencyOfWord {
       public static void main(String[] args) {
-        String s=" I scream you scream we all scream for ice cream";
+        String s="I scream you scream we all scream for ice cream";
         Map<String,Integer> map=new LinkedHashMap<>();
         for(String str:s.split(" ")){
            map.put(str,map.getOrDefault(str,0)+1);
