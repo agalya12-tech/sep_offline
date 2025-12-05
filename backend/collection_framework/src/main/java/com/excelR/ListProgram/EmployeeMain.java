3e282a343206e3e53fdf7ae3df7f9981558937fe
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-05 14:28:44 +0530

    comparable and comparator notes

--- backend/collection_framework/src/main/java/com/excelR/ListProgram/EmployeeMain.java
+++ backend/collection_framework/src/main/java/com/excelR/ListProgram/EmployeeMain.java
@@ -1,9 +1,19 @@
 package com.excelR.ListProgram;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class EmployeeMain {
     public static void main(String[] args) {
+
+//        List<String>li=new ArrayList<>(Arrays.asList("Alice","David","Bob","Carol"));
+//        System.out.println(li);
+//        Collections.sort(li); // ascending order
+//        System.out.println(li);
+
+
+
+
         List<Employee> list= new ArrayList<>();
         list.add( new Employee("Alice",345465654));
         list.add( new Employee("Emil",785434455));
@@ -11,5 +21,7 @@ public class EmployeeMain {
         list.add( new Employee("Bob",345345234));
         list.add( new Employee("David",56787454));
         list.add( new Employee("Carl",995645445));
+
+
     }
 }
