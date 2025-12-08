d495d968050caff13316d1c8cf1cc2dba7f20561
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-08 14:38:43 +0530

    java 8 features notes

---
+++ backend/collection_framework/src/main/java/com/excelR/java8Features/OptionalClassProgram.java
@@ -0,0 +1,31 @@
+package com.excelR.java8Features;
+
+import java.util.Optional;
+
+public class OptionalClassProgram {
+    public static void main(String[] args) {
+//      int size=getName().length();
+//      System.out.println(size);
+
+        Optional<String> res = getName();
+        if(res.isPresent()){
+            String name = res.get();
+            System.out.println(name.length());
+        }else{
+        System.out.println("can't find size for null values");
+        }
+    }
+
+    public static Optional<String> getName(){
+        String name="radha";
+//        return Optional.of(String.valueOf(name));
+        return Optional.ofNullable(name);
+    }
+
+    
+    //    public static String getName(){
+//        String name=null;
+//        return name;
+//    }
+
+}
