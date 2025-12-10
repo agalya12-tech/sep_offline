46fa033877f403911d354921f16918e94aeebfe6
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-10 13:24:31 +0530

    jdbc

--- backend/collection_framework/src/main/java/com/excelR/ListProgram/SingleLinkedListProgram.java
+++ backend/collection_framework/src/main/java/com/excelR/ListProgram/SingleLinkedListProgram.java
@@ -59,35 +59,8 @@ class SinglyLinkedList<T> {
     }
 
 
-    public T get(int index){
-        if(index<0||index>size){ throw new IndexOutOfBoundsException("index out of bounds"); }
-        Node<T>current=head;
-        for(int i=0;i<index;i++){
-            current=current.next;
-        }
-        return current.data;
-    }
-    public void print(){
-        if(isEmpty()){ System.out.println("List is empty");  }
-        else{
-            Node<T>temp=head;
-            while(head!=null){
-                System.out.print(head.data+"->");
-                head=head.next;
-            }
-            head=temp;
-            System.out.println("null");
-        }
 
-    }
-    public void set(int index, T data) {
-        if (index<0 || index>size){ throw new IndexOutOfBoundsException("index out of bounds"); }
-        else{
-            Node<T> current = head;
-            for (int i = 0; i < index; i++) {
-                current = current.next;
-            }
-            current.data = data;}
-    }
+
+   
 
 }
