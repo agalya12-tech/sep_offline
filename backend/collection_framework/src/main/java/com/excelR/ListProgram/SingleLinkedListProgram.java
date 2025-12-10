733965f5e76f2586bef754940ecd412b8d2fddce
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-10 13:16:11 +0530

    jdbc

--- backend/collection_framework/src/main/java/com/excelR/ListProgram/SingleLinkedListProgram.java
+++ backend/collection_framework/src/main/java/com/excelR/ListProgram/SingleLinkedListProgram.java
@@ -2,57 +2,71 @@ package com.excelR.ListProgram;
 
 public class SingleLinkedListProgram {
     public static void main(String[] args) {
-         SinglyLinkedList<Integer> list=new SinglyLinkedList<Integer>();
-         System.out.println(list.size());
-         System.out.println(list.isEmpty());
-         list.add(1);
-         list.add(2);
-         list.add(2);
-         list.add(2);
-         list.add(2);
-         list.add(2);
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
         System.out.println(list.size());
         System.out.println(list.isEmpty());
-
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list.size());
+        System.out.println(list.isEmpty());
+        list.print();
 
 
     }
 }
 
-class Node<T>{
+class Node<T> {
     T data;
     Node<T> next;
-    public Node(T data){
-        this.data=data;
-        this.next=null;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
     }
 
 }
 
 
-class SinglyLinkedList<T>{
-    Node<T>head;
+class SinglyLinkedList<T> {
+    Node<T> head;
     int size;
     T data;
 
-    public int size(){
+    public int size() {
         return size;
     }
-    public boolean isEmpty(){
-        return size==0;
+
+    public boolean isEmpty() {
+        return size == 0;
     }
-    public void add(T data){
-        Node<T>current=new  Node<T>(data);
-        if(head==null){
-            head=current;
-        }else{
-            Node<T> temp=head;
-            while(temp.next!=null){
-                temp=temp.next;
+
+    public void add(T data) {
+        Node<T> current = new Node<T>(data);
+        if (head == null) {
+            head = current;
+        } else {
+            Node<T> temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
             }
-            temp.next=current;
+            temp.next = current;
         }
         size++;
     }
 
+    public void print() {
+        if (isEmpty()) {
+            System.out.println("List is empty");
+        } else {
+            Node<T> temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + "->");
+                temp = temp.next;
+            }
+            System.out.println("null");
+
+
+        }
+    }
 }
