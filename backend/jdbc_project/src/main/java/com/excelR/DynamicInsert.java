833d1daf7f1b4b0edf61c98e5e70d8c9fcb57129
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-10 16:56:36 +0530

    jdbc

--- backend/jdbc_project/src/main/java/com/excelR/DynamicInsert.java
+++ backend/jdbc_project/src/main/java/com/excelR/DynamicInsert.java
@@ -1,5 +1,28 @@
 package com.excelR;
 
-public class DynamicInsert {
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
 
+import com.mysql.cj.jdbc.Driver;
+
+public class DynamicInsert {
+ public static void main(String[] args) throws SQLException {
+	Scanner sc=new Scanner(System.in);
+	System.out.print("Enter toy id : ");
+	int id=sc.nextInt();
+	System.out.print("Enter toy name : ");
+	String name=sc.next();
+	System.out.print("Enter toy price : ");
+	double price=sc.nextDouble();
+	System.out.print("Enter toy qunatity : ");
+	int quantity=sc.nextInt();
+	
+	//load and register
+	DriverManager.registerDriver(new Driver());
+    //Establish the connection
+	DriverManager.getConnection
+		("jdbc:mysql://localhost:3306/database1?user=root&password=root");
+	
+}
 }
