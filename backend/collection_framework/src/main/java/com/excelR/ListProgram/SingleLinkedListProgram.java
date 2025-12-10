36238f07a45ec670ffa2f5c38d267a316d081811
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-10 13:23:33 +0530

    jdbc

--- backend/collection_framework/src/main/java/com/excelR/ListProgram/SingleLinkedListProgram.java
+++ backend/collection_framework/src/main/java/com/excelR/ListProgram/SingleLinkedListProgram.java
@@ -5,12 +5,15 @@ public class SingleLinkedListProgram {
         SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
         System.out.println(list.size());
         System.out.println(list.isEmpty());
-        list.add(1);
+        list.add(11);
         list.add(2);
         list.add(3);
         System.out.println(list.size());
         System.out.println(list.isEmpty());
-        list.print();
+
+
+
+
 
 
     }
@@ -55,18 +58,36 @@ class SinglyLinkedList<T> {
         size++;
     }
 
-    public void print() {
-        if (isEmpty()) {
-            System.out.println("List is empty");
-        } else {
-            Node<T> temp = head;
-            while (temp != null) {
-                System.out.print(temp.data + "->");
-                temp = temp.next;
-            }
-            System.out.println("null");
 
-
+    public T get(int index){
+        if(index<0||index>size){ throw new IndexOutOfBoundsException("index out of bounds"); }
+        Node<T>current=head;
+        for(int i=0;i<index;i++){
+            current=current.next;
         }
+        return current.data;
     }
+    public void print(){
+        if(isEmpty()){ System.out.println("List is empty");  }
+        else{
+            Node<T>temp=head;
+            while(head!=null){
+                System.out.print(head.data+"->");
+                head=head.next;
+            }
+            head=temp;
+            System.out.println("null");
+        }
+
+    }
+    public void set(int index, T data) {
+        if (index<0 || index>size){ throw new IndexOutOfBoundsException("index out of bounds"); }
+        else{
+            Node<T> current = head;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+            current.data = data;}
+    }
+
 }
