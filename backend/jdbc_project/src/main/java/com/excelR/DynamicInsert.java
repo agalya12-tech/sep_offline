e1281a93111709585068e8693dc8a792f6ad8e0c
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-11 14:19:57 +0530

    uploading jdbc notes

--- backend/jdbc_project/src/main/java/com/excelR/DynamicInsert.java
+++ backend/jdbc_project/src/main/java/com/excelR/DynamicInsert.java
@@ -1,13 +1,15 @@
 package com.excelR;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
 import com.mysql.cj.jdbc.Driver;
 
 public class DynamicInsert {
- public static void main(String[] args) throws SQLException {
+ public static void main(String[] args)  {
 	Scanner sc=new Scanner(System.in);
 	System.out.print("Enter toy id : ");
 	int id=sc.nextInt();
@@ -17,12 +19,30 @@ public class DynamicInsert {
 	double price=sc.nextDouble();
 	System.out.print("Enter toy qunatity : ");
 	int quantity=sc.nextInt();
-	
+	 try {
 	//load and register
 	DriverManager.registerDriver(new Driver());
     //Establish the connection
-	DriverManager.getConnection
+	Connection con = DriverManager.getConnection
 		("jdbc:mysql://localhost:3306/database1?user=root&password=root");
+    //creation of statements
+	PreparedStatement ps=con.prepareStatement("insert into toys values(?,?,?,?)");
+	ps.setInt(1, id);
+	ps.setString(2, name);
+	ps.setDouble(3,price );
+	ps.setInt(4, quantity);
+    //execution of statement
+	ps.execute();
+   //close
+	ps.close();
+	con.close();
+	
+	System.out.println("Inserted "+name);}
+	 catch (Exception e) {
+		 System.err.println(e.getMessage());
+	}
+	
+	
 	
 }
 }
