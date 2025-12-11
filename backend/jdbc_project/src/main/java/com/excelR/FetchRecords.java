13677a6551d67a5bc8474fb03f633d1e8889544f
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-11 15:36:35 +0530

    uploading jdbc notes

--- backend/jdbc_project/src/main/java/com/excelR/FetchRecords.java
+++ backend/jdbc_project/src/main/java/com/excelR/FetchRecords.java
@@ -7,17 +7,23 @@ import com.mysql.cj.jdbc.Driver;
 
 public class FetchRecords {
 	public static void main(String[] args) throws SQLException {
-		String query="select * from toys";
-		DriverManager.registerDriver(new Driver());
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/database1","root","root");
-	    Statement s=con.createStatement();
-	  
+		String query="select * from toys where id=1 and name='superman'";
+		DriverManager.registerDriver(new Driver()); 
+		Connection con = DriverManager.getConnection
+				("jdbc:mysql://localhost:3306/database1","root","root");
+//	    Statement s=con.createStatement();
+		PreparedStatement ps=con.prepareStatement(query);
 //	    System.out.println(s.execute(query));
-	    System.out.println(s.executeQuery(query));
-
+	    ResultSet rs = ps.executeQuery();
+        if(rs.next()) {
+        	System.out.println("ID : "+rs.getInt("id"));
+        	System.out.println("Name : "+rs.getString("name"));
+        	System.out.println("Price : "+rs.getDouble("price"));
+        	System.out.println("Quanity : "+rs.getInt("quantity"));
+        	System.out.println("====================");
+        }
 	    s.close();
 	    con.close();
-	
 	}
 
 }
