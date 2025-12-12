96e8644a97fe10c8477feb7695927d108213c161
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-12 16:28:38 +0530

    servlets notes

---
+++ backend/servlet_project/src/main/java/servlet_project/controller/SaveToy.java
@@ -0,0 +1,18 @@
+package servlet_project.controller;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+@WebServlet("/save")
+public class SaveToy extends GenericServlet {
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+    		
+	}
+
+}
