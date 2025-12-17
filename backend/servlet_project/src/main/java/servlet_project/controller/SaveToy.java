5e894bac744150014a4612bd2b2f969a1f4cca7d
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-17 16:00:38 +0530

    uploading servlet

--- backend/servlet_project/src/main/java/servlet_project/controller/SaveToy.java
+++ backend/servlet_project/src/main/java/servlet_project/controller/SaveToy.java
@@ -4,12 +4,14 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 import javax.servlet.GenericServlet;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 
 import servlet_project.dao.ToyCrud;
+import servlet_project.dto.Toy;
 @WebServlet("/save")
 public class SaveToy extends GenericServlet {
 
@@ -21,12 +23,18 @@ public class SaveToy extends GenericServlet {
     	String quantity=req.getParameter("quantity");	
     	ToyCrud crud=new ToyCrud();
     	try {
-			if(crud.insertToy(Integer.parseInt(id),
-					name,Double.parseDouble(price) , Integer.parseInt(quantity))>0) {
-				 System.out.println(name +" saved successfully");
+    		Toy toy=new Toy();
+    		toy.setId(Integer.parseInt(id));
+    		toy.setName(name);
+    		toy.setPrice(Double.parseDouble(price));
+    		toy.setQuantity(Integer.parseInt(quantity));
+			if(crud.insertToy(toy)>0) {
+				RequestDispatcher dispatcher=req.getRequestDispatcher("dashboard.jsp");
+				dispatcher.forward(req, res);
 			}
 		} catch (SQLException e) {
-			System.out.println(e.getMessage());
+			RequestDispatcher dispatcher=req.getRequestDispatcher("saveToy.html");
+			dispatcher.include(req, res);
 		}
 
 
--- backend/servlet_project/src/main/java/servlet_project/dao/ToyCrud.java
+++ backend/servlet_project/src/main/java/servlet_project/dao/ToyCrud.java
@@ -4,16 +4,19 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import com.mysql.cj.jdbc.Driver;
+
+import servlet_project.dto.Toy;
 public class ToyCrud {
-	public int insertToy(int id, String name,double price,int quantity) throws SQLException {
+	public int insertToy(Toy toy) throws SQLException {
 		DriverManager.registerDriver(new Driver());
 		Connection con = DriverManager.getConnection
 			("jdbc:mysql://localhost:3306/database1?user=root&password=root");
-		PreparedStatement ps=con.prepareStatement("insert into toys values(?,?,?,?)");
-		ps.setInt(1, id);
-		ps.setString(2, name);
-		ps.setDouble(3,price );
-		ps.setInt(4, quantity);
+		PreparedStatement ps=con.prepareStatement
+				("insert into toys values(?,?,?,?)");
+		ps.setInt(1, toy.getId());
+		ps.setString(2, toy.getName());
+		ps.setDouble(3,toy.getPrice() );
+		ps.setInt(4, toy.getQuantity());
 		int res=ps.executeUpdate();
 		ps.close();
 		con.close();
