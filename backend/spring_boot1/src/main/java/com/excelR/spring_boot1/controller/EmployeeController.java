a1daa148a94423418dc6a407fd2b78cbe6b0ffc6
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-22 15:33:52 +0530

    spring  notes

--- backend/spring_boot1/src/main/java/com/excelR/spring_boot1/controller/EmployeeController.java
+++ backend/spring_boot1/src/main/java/com/excelR/spring_boot1/controller/EmployeeController.java
@@ -1,8 +1,22 @@
 package com.excelR.spring_boot1.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller
+//@ResponseBody
+
+@RestController
 public class EmployeeController {
-
+//	http://localhost:8080/testCase1
+    @RequestMapping("/testCase1")
+	public void testCase1() {
+    System.out.println("Test Case1 is working ....!!");
+	}
+    
+//	http://localhost:8080/testCase2
+    @RequestMapping("/testCase2")
+    public String testCase2() {
+    	return "Test case2 is working";
+    }
 }
--- backend/spring_boot1/src/main/java/com/excelR/spring_boot1/dto/Employee.java
+++ backend/spring_boot1/src/main/java/com/excelR/spring_boot1/dto/Employee.java
@@ -2,7 +2,18 @@ package com.excelR.spring_boot1.dto;
 
 import org.springframework.stereotype.Component;
 
-@Component
-public class Employee {
+import lombok.*;
 
+@Data
+@Component
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+  
+	private int id;
+	private String name;
+	private String email;
+	private long phone;
+	private String dept;
+	
 }
