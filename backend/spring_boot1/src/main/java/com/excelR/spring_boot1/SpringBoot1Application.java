500a3b5b67d2ededa5775069cda41511310a89ed
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-22 15:05:49 +0530

    spring  notes

--- backend/spring_boot1/src/main/java/com/excelR/spring_boot1/SpringBoot1Application.java
+++ backend/spring_boot1/src/main/java/com/excelR/spring_boot1/SpringBoot1Application.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import com.excelR.spring_boot1.controller.EmployeeController;
+import com.excelR.spring_boot1.dao.EmployeeDao;
 import com.excelR.spring_boot1.dto.Employee;
+import com.excelR.spring_boot1.service.EmployeeService;
 
 @SpringBootApplication
 public class SpringBoot1Application {
@@ -13,6 +16,12 @@ public class SpringBoot1Application {
 		ApplicationContext ac=SpringApplication.run(SpringBoot1Application.class, args);
 		System.out.println("Hello world");
 		System.out.println(ac.getBean(Employee.class));
+		System.out.println(ac.getBean(EmployeeDao.class));
+		System.out.println(ac.getBean(EmployeeService.class));
+		System.out.println(ac.getBean(EmployeeController.class));
+
+
+
 	}
 
 }
