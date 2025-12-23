612ff229cbfb63ddba0f8da85d7825d73e0383d7
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-23 16:16:06 +0530

    uploading notes

--- backend/spring_boot1/src/main/java/com/excelR/spring_boot1/repository/EmployeeRepo.java
+++ backend/spring_boot1/src/main/java/com/excelR/spring_boot1/repository/EmployeeRepo.java
@@ -1,9 +1,15 @@
 package com.excelR.spring_boot1.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.excelR.spring_boot1.dto.Employee;
 
 public interface EmployeeRepo  extends JpaRepository<Employee, Integer>{
 
+//	select * from employee where email=?   -- SQL Query
+	 @Query("select e from Employee e where email=?1")//---JPQL Query
+	 Optional<Employee>findByEmail(String email);
 }
