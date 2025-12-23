c3deac5c8c335310bb582eaa5e570a436a2e8cb4
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-23 13:59:15 +0530

    uploading notes

--- backend/spring_boot1/src/main/java/com/excelR/spring_boot1/dao/EmployeeDao.java
+++ backend/spring_boot1/src/main/java/com/excelR/spring_boot1/dao/EmployeeDao.java
@@ -1,8 +1,19 @@
 package com.excelR.spring_boot1.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.excelR.spring_boot1.dto.Employee;
+import com.excelR.spring_boot1.repository.EmployeeRepo;
 
 @Repository
 public class EmployeeDao {
-
+      
+	@Autowired
+	EmployeeRepo repo;
+	
+	public Employee saveEmployee(Employee e) {
+		 return  repo.save(e);
+	} 
 }
+
--- backend/spring_boot1/src/main/java/com/excelR/spring_boot1/repository/EmployeeRepo.java
+++ backend/spring_boot1/src/main/java/com/excelR/spring_boot1/repository/EmployeeRepo.java
@@ -2,6 +2,8 @@ package com.excelR.spring_boot1.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.excelR.spring_boot1.dto.Employee;
+
 public interface EmployeeRepo  extends JpaRepository<Employee, Integer>{
 
 }
