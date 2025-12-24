e8b4b2c8147a6cd0c843473c7bb758a0e4af3922
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-24 14:45:06 +0530

    uploading notes.txt

--- backend/employee_boot/src/components/Register.jsx
+++ backend/employee_boot/src/components/Register.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const Register = () => {
+    let nameRef = useRef()
+    let emailRef = useRef()
+    let phoneRef = useRef()
+    let deptRef = useRef()
   return (
-    <div>Register</div>
+   <>
+    <div className="container">
+      <h2>Register Form</h2>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" ref={nameRef} /> 
+         <br />
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" ref={emailRef} />
+        <br />
+          <label htmlFor="phone">Phone:</label>
+          <input type="tel" id="phone" ref={phoneRef} />
+         <br />
+          <label htmlFor="dept">Department:</label>
+          <input type="text" id="dept" ref={deptRef} />
+         <br />
+        <button  className="btn btn-primary">Register</button>
+    </div>
+
+   </>
   )
 }
 
