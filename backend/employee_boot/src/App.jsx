b2ca15bb25ec8c37e8310fd7ab82e19255c66f8e
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-24 16:31:54 +0530

    uploading notes.txt

--- backend/employee_boot/src/App.jsx
+++ backend/employee_boot/src/App.jsx
@@ -4,11 +4,13 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dashboard from './components/Dashboard'
 const App = () => {
   return (
    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   )
--- backend/employee_boot/src/services/api.js
+++ backend/employee_boot/src/services/api.js
@@ -5,3 +5,9 @@ export const employeeRegister = async (employeeData) => {
   let response = await axios.post(`${API}/save`, employeeData);
   return response;
 };
+
+
+export const fetchAllEmployees=async()=>{
+    let response=await axios.get(`${API}/fetchAll`)
+    return response;
+}
