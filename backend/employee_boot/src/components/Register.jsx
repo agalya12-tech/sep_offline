3ecbec38201abcfc4c9cb29cf5f0b6d1f8d85a3b
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-24 14:46:06 +0530

    uploading notes.txt

--- backend/employee_boot/src/components/Register.jsx
+++ backend/employee_boot/src/components/Register.jsx
@@ -11,16 +11,16 @@ const Register = () => {
       <h2>Register Form</h2>
           <label htmlFor="name">Name:</label>
           <input type="text" id="name" ref={nameRef} /> 
-         <br />
+         <br /> <br />
           <label htmlFor="email">Email:</label>
           <input type="email" id="email" ref={emailRef} />
-        <br />
+        <br /> <br />
           <label htmlFor="phone">Phone:</label>
           <input type="tel" id="phone" ref={phoneRef} />
-         <br />
+         <br /> <br />
           <label htmlFor="dept">Department:</label>
           <input type="text" id="dept" ref={deptRef} />
-         <br />
+         <br /> <br />
         <button  className="btn btn-primary">Register</button>
     </div>
 
