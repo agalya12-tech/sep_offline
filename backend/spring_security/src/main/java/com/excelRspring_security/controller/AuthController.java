e5e20c61a6034e4c26db2292fd84b98c49c591c5
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-29 15:39:46 +0530

    upload

--- backend/spring_security/src/main/java/com/excelRspring_security/controller/AuthController.java
+++ backend/spring_security/src/main/java/com/excelRspring_security/controller/AuthController.java
@@ -1,5 +1,24 @@
 package com.excelRspring_security.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.excelRspring_security.dao.UserDao;
+import com.excelRspring_security.entity.User;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
 
+	@Autowired
+	UserDao dao;
+//	 http://localhost:8080/auth/register
+	@PostMapping("/register")
+	public ResponseEntity<User>registerUser(User user){
+		return new ResponseEntity<User>(dao.saveUser(user), HttpStatus.CREATED);
+	}
 }
