3208729bd0f8df80f4f30885cc2464365e76b022
Excelr Solutions <mailmeasradha@gmail.com>
2025-12-30 14:53:28 +0530

    uploading notes.txt

--- backend/spring_security/src/main/java/com/excelRspring_security/repository/UserRepo.java
+++ backend/spring_security/src/main/java/com/excelRspring_security/repository/UserRepo.java
@@ -1,9 +1,13 @@
 package com.excelRspring_security.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.excelRspring_security.entity.User;
 
 public interface UserRepo  extends JpaRepository<User, Integer>{
-
+ 
+	Optional<User>findByEmail(String email);
 }
+
