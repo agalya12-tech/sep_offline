43ea338cd6a479f1da7ff4df6fc64efae092f0b4
Excelr Solutions <mailmeasradha@gmail.com>
2026-01-02 15:06:28 +0530

    uploading notes.txt

--- backend/spring_security_frontend/src/components/Register.jsx
+++ backend/spring_security_frontend/src/components/Register.jsx
@@ -1,32 +1,45 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { registerUser } from '../services/AuthService';
 
 const Register = () => {
-    let nameRef=React.useRef();
-    let emailRef=React.useRef();
-    let passwordRef=React.useRef();
-    let phoneRef=React.useRef();
-    let roleRef=React.useRef();
-  let handleSubmit=()=>{
-    let user={
-        name:nameRef.current.value,
-        email:emailRef.current.value,
-        password:passwordRef.current.value,
-        phone:phoneRef.current.value,
-        role:roleRef.current.value
+  let nameRef = React.useRef();
+  let emailRef = React.useRef();
+  let passwordRef = React.useRef();
+  let phoneRef = React.useRef();
+  let roleRef = React.useRef();
+  let navigate = useNavigate();
+  let handleSubmit = () => {
+    let user = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      phone: phoneRef.current.value,
+      role: roleRef.current.value
     }
-    console.log(user);
+
+    registerUser(user)
+      .then((res) => {
+        if (res.status === 201) {
+          alert("Registration Successful");
+          navigate('/login');
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Registration Failed");
+      });
   }
 
-  let   handleOauth=(provider)=>{
-    window.location.href=`http://localhost:8080/oauth2/authorization/${provider}`;
+  let handleOauth = (provider) => {
+    window.location.href = `http://localhost:8080/oauth2/authorization/${provider}`;
   }
   return (
     <>
 
       <h3>Register Account with </h3>
-      <button onClick={()=>{handleOauth('google')} }>Google</button>
-      <button onClick={()=>{handleOauth('github')}}>Github</button>
+      <button onClick={() => { handleOauth('google') }}>Google</button>
+      <button onClick={() => { handleOauth('github') }}>Github</button>
 
 
       <h1>Register Component</h1>
@@ -42,7 +55,7 @@ const Register = () => {
       <button className='btn btn-outline-success' onClick={handleSubmit}>Register</button>
 
 
-      
+
     </>
 
   )
